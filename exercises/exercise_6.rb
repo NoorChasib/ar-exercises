fd57f064ba9b7d7f17252d8a2a06f8eb2dd29836
Noor Chasib <noorchasib@gmail.com>
2023-01-25 15:28:00 -0800

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,11 +1,52 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
 
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60,
+)
+@store1.employees.create(
+  first_name: "John", 
+  last_name: "Doe", 
+  hourly_rate: 40
+)
+@store1.employees.create(
+  first_name: "Jane", 
+  last_name: "Doe", 
+  hourly_rate: 40
+)
+@store1.employees.create(
+  first_name: "Bob", 
+  last_name: "Smith", 
+  hourly_rate: 40
+)
+
+
+@store2.employees.create(
+  first_name: "Sally",
+  last_name: "Smith",
+  hourly_rate: 40,
+)
+@store2.employees.create(
+  first_name: "Sue", 
+  last_name: "Smith", 
+  hourly_rate: 40
+)
+@store2.employees.create(
+  first_name: "Joe", 
+  last_name: "Smith", 
+  hourly_rate: 40
+)
+@store2.employees.create(
+  first_name: "Jim", 
+  last_name: "Smith", 
+  hourly_rate: 40
+)
